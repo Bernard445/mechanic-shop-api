1e473a1b72f6a0f33d657e33f9158e11421306cb
Baruchadress <baruchadress@gmail.com>
2026-02-03 14:13:20 -0500

    fix production config loading

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,3 @@
 from app import create_app
-from app.models import db
 
-
-app = create_app('ProductionConfig')
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+app = create_app("ProductionConfig")
