886493b304719a71cfe588b7a732ab764bd97c4d
Baruchadress <baruchadress@gmail.com>
2026-02-03 13:43:38 -0500

    fix create_app for production config

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from app import create_app
 from app.models import db
 
 
-app = create_app('productionconfig')
+app = create_app('ProductionConfig')
 
 if __name__ == "__main__":
     with app.app_context():
